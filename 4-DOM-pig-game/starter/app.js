8ba1fd6075205903175bdaa3eb3d4c70e94e00d8
JasonZheng <hbxzknight@gmail.com>
2019-09-27 23:20:51 +1000

    4.2 events and event handling;

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,13 +9,81 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
 
-// init view
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+
+newGame(); // init view
+
+// events and event handling 
+document.querySelector('.btn-new').addEventListener('click', newGame);
+document.querySelector('.btn-roll').addEventListener('click', roll);
+document.querySelector('.btn-hold').addEventListener('click', hold);
+
+function newGame() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+
+function roll() {
+    var dice_number = Math.ceil(Math.random() * 6);
+
+    // update view
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice_number + '.png'; 
+
+    // update the round score If the rolled number was NOT a 1
+    if (dice_number > 1) {
+        // Add current score
+        roundScore += dice_number;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        nextPlayer();
+    }
+}
+
+function nextPlayer() {
+    activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+
+function hold() {
+    // Add current score to global score
+    scores[activePlayer] += roundScore;
+    
+    // update UI
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    // check if player won the game
+    if (scores[activePlayer] >= 30) {
+        document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        console.log(document.querySelector('.dice').style.display);
+        document.querySelector('.player-'+ activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-'+ activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+}
