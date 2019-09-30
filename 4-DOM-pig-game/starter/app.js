e3d37d9f8be078ee2e9b4c37932a71ebae8d0bbb
JasonZheng <hbxzknight@gmail.com>
2019-09-30 14:12:55 +1000

    [feature] challenge 3

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -14,7 +14,7 @@ YOUR 3 CHALLENGES
 Change the game to follow these rules:
 1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable) 
 2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-[TODO] 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 
 
@@ -26,6 +26,8 @@ var winningBar = 10;
 
 // view elements
 var inputField = document.querySelector('.winning-score');
+var dice0 = document.getElementById('dice-0');
+var dice1 = document.getElementById('dice-1');
 
 
 newGame(); // init view
@@ -41,7 +43,8 @@ function newGame() {
     activePlayer = 0;
     winningBar = inputField.value;
 
-    document.querySelector('.dice').style.display = 'none';
+    dice0.style.display = 'none';
+    dice1.style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
@@ -62,19 +65,20 @@ function roll() {
         return;
     }
 
-    var diceNumber = Math.ceil(Math.random() * 6);
+    var dice0Number = Math.ceil(Math.random() * 6);
+    var dice1Number = Math.ceil(Math.random() * 6);
 
     // update view
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + diceNumber + '.png'; 
+    dice0.style.display = 'block';
+    dice1.src = 'dice-' + dice1Number + '.png'; 
+    dice1.style.display = 'block';
+    dice0.src = 'dice-' + dice0Number + '.png'; 
 
     // update the round score If the rolled number was NOT a 1
-    if (diceNumber == 1 || (diceNumber == 6 && lastDice == 6)) {
+    if (dice0Number == 1 || dice1Number == 1) {
         nextPlayer();
     } else {
-        lastDice = diceNumber;
-        roundScore += diceNumber;
+        roundScore += dice0Number + dice1Number;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     }
 }
@@ -82,7 +86,6 @@ function roll() {
 function nextPlayer() {
     activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
-    lastDice = -1;
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
@@ -90,7 +93,8 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active')
     document.querySelector('.player-1-panel').classList.toggle('active')
 
-    document.querySelector('.dice').style.display = 'none';
+    dice0.style.display = 'none';
+    dice1.style.display = 'none';
 }
 
 
@@ -110,7 +114,8 @@ function hold() {
 
     function gameEnd() {
         document.getElementById('name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
+        dice0.style.display = 'none';
+        dice1.style.display = 'none';
         console.log(document.querySelector('.dice').style.display);
         document.querySelector('.player-'+ activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-'+ activePlayer + '-panel').classList.remove('active');
